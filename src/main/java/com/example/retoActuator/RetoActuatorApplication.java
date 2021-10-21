f85e127c55ca4adeaa867212fa5c12c5c452ea38
adamY97 <adam24891@gmail.com>
2021-10-21 12:28:57 +0200

    Initial commit

---
+++ src/main/java/com/example/retoActuator/RetoActuatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.retoActuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetoActuatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetoActuatorApplication.class, args);
+	}
+
+}
